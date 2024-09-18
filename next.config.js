d9ed995f7e1de7f8a2d09c7eb6fee77ac52fd988
vijiRajhamohan <81389688+vijiRajhamohan@users.noreply.github.com>
2024-09-18 16:08:36 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+// {
+//   images: {
+//     domains: ['images.unplash.com']
+//   }
+// }
